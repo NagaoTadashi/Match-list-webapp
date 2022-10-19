792caad35f29cc8d33517a789ae84ff132781ecb
NagaoTadashi <tahofu89@gmail.com>
2022-10-19 14:35:53 +0000

    complete new create edit update destroy method

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,4 +4,41 @@ class MatchesController < ApplicationController
     @matches = Match.all
   end 
   
+  def new
+    @match = Match.new
+  end
+  
+  def create
+    @match = Match.new(match_params)
+    if @match.save
+      redirect_to root_path
+    else 
+      render 'new',status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @match = Match.find(params[:id])
+  end
+  
+  def update
+    @match = Match.find(params[:id])
+    if @match.update(match_params)
+      redirect_to root_path
+    else
+      render 'edit',status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @match = Match.find(params[:id])
+    @match.destroy
+    redirect_to root_path, status: :see_other
+  end
+  
+  private
+  def match_params
+    params.require(:match).permit(:Sec,:Date,:Venue,:Oppnent)
+  end  
+  
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,6 @@
 class Match < ApplicationRecord
+    validates :Sec, presence: true
+    validates :Date, presence: true
+    validates :Venue, presence: true
+    validates :Oppnent, presence: true
 end
